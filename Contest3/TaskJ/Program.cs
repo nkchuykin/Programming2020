8916b01082e032d209036d1e2c23d39d896d4cd1
nkchuykin <nkchuykin@edu.hse.ru>
2020-10-06 23:36:29 +0300

    Updated Program.cs

--- Contest3/TaskJ/Program.cs
+++ Contest3/TaskJ/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 partial class Program
@@ -7,7 +8,7 @@ partial class Program
     {
         if (!ValidateQuery(File.ReadAllLines("query.txt")[0], out string[] queryParameters))
         {
-            Console.WriteLine("Incorrect input");
+            File.WriteAllLines("queryResult.txt", new List<string> {"Incorrect input"});
             return;
         }
         
