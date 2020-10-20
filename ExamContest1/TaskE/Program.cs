8ef8e06e92d6762d45f6fbef10dd833c436f34c7
nchuykin <nkchuykin@edu.hse.ru>
2020-10-20 12:03:58 +0300

    Fix mistake  in Task E.

--- ExamContest1/TaskE/Program.cs
+++ ExamContest1/TaskE/Program.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 
-    public partial class Program
+public partial class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int length;
+        if (!int.TryParse(Console.ReadLine(), out length) || !IsArrayLengthCorrect(length))
         {
-            int length;
-            if (!ParseInput(Console.ReadLine(), out length) || !IsArrayLengthCorrect(length))
-            {
-                Console.WriteLine("Incorrect input");
-                return;
-            }
-
-            int[] arr;
-            GenerateArray(length, out arr);
-            double average = GetArrayAverage(arr);
-            Console.WriteLine(GetSumOfNumbersLessThanAverage(arr, average));
+            Console.WriteLine("Incorrect input");
+            return;
         }
+        int[] arr;
+        if(!GenerateArray(length, out arr))
+        {
+            Console.WriteLine("Incorrect input");
+            return;
+        }
+        double average = GetArrayAverage(arr);
+        Console.WriteLine(GetSumOfNumbersLessThanAverage(arr, average));
     }
+}
