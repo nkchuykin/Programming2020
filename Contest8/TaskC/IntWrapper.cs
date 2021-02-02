b3c3c7cc7f32d223bac5d7979cf66dfd28b3e85c
nchuykin <nkchuykin@edu.hse.ru>
2021-02-02 16:41:55 +0300

    Fix errors in TaskC settings and template

--- Contest8/TaskC/IntWrapper.cs
+++ Contest8/TaskC/IntWrapper.cs
@@ -2,6 +2,13 @@
 
 public class IntWrapper
 {
+    private int number;
+
+    public int Number
+    {
+        get { return number; }
+    }
+
     public IntWrapper(int number)
     {
         throw new NotImplementedException();
--- Contest8/TaskC/Program.cs
+++ Contest8/TaskC/Program.cs
@@ -6,16 +6,15 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var number = new IntWrapper(int.Parse(Console.ReadLine()));
+        IntWrapper number = new IntWrapper(int.Parse(Console.ReadLine()));
         Del del = number.FindNumberLength;
-        
         try
         {
             Console.WriteLine(del());
         }
         catch (ArgumentException e)
         {
-            Console.WriteLine(e.GetType());
+            Console.WriteLine(e.Message);
         }
     }
 }
