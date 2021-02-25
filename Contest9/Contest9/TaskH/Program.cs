2cf97a46ad780a135813644f7bb4a7f5abeb9b20
nchuykin <nkchuykin@edu.hse.ru>
2021-02-25 16:05:09 +0300

    Fixed template for task H.

--- Contest9/Contest9/TaskH/Program.cs
+++ Contest9/Contest9/TaskH/Program.cs
@@ -4,63 +4,6 @@ using System.IO;
 
 class MainClass
 {
-    private static string[] coms = {"create ", "add ", "remove "};
-
-    private static string[] permissions =
-    {
-        "UserRead",
-        "UserWrite",
-        "UserExecute",
-        "GroupRead",
-        "GroupWrite",
-        "GroupExecute",
-        "EveryoneRead",
-        "EveryoneWrite",
-        "EveryoneExecute"
-    };
-
-    private static string RandomString()
-    {
-        var line = string.Empty;
-        for (int i = 0; i < 5; i++)
-            line += (char) Random.Next('a', 'z' + 1);
-        return line;
-    }
-
-    private static readonly Random Random = new Random();
-
-    private static void MakeTests()
-    {
-        for (int test = 3; test <= 30; test++)
-        {
-            using (StreamWriter sw = new StreamWriter("tests/" + test.ToString("d2")))
-            {
-                List<string> files = new List<string>();
-                int n = Random.Next(50, 100);
-                sw.WriteLine(n);
-                for (int i = 0; i < n; i++)
-                {
-                    string command;
-                    do
-                        command = coms[Random.Next(3)];
-                    while (command != "create " && files.Count == 0);
-                    string file;
-                    switch (command)
-                    {
-                        case "create ":
-                            file = RandomString();
-                            files.Add(file);
-                            sw.WriteLine(command + file);
-                            break;
-                        default:
-                            file = files[Random.Next(files.Count)];
-                            sw.WriteLine(command + file + " " + permissions[Random.Next(9)]);
-                            break;
-                    }
-                }
-            }
-        }
-    }
 
     public static void Main(string[] args)
     {
